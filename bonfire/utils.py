efbf6d48390c902ae6651073c10a29d686e06395
Valentin Krasontovitsch <valkra@ihelse.net>
2018-05-08 11:15:53 +0200

    Feat: Read password from config file

--- bonfire/utils.py
+++ bonfire/utils.py
@@ -22,6 +22,9 @@ def api_from_config(cfg, node_name="default"):
     host = cfg.get(section_name, "host")
     port = cfg.get(section_name, "port")
     endpoint = cfg.get(section_name, "endpoint")
+    password = None
+    if cfg.has_option(section_name, "password"):
+        password = cfg.get(section_name, "password")
 
     if cfg.has_option(section_name, "username"):
         username = cfg.get(section_name, "username")
@@ -42,8 +45,9 @@ def api_from_config(cfg, node_name="default"):
     if cfg.has_option(section_name, "default_stream"):
         default_stream = cfg.get(section_name, "default_stream")
 
-    return GraylogAPI(host=host, port=port, endpoint=endpoint, username=username, default_stream=default_stream,
-                      scheme=scheme, proxies=proxies)
+    return GraylogAPI(host=host, port=port, endpoint=endpoint,
+            username=username, password=password,
+            default_stream=default_stream, scheme=scheme, proxies=proxies)
 
 
 def api_from_host(host, port, endpoint, username, scheme, proxies=None):
